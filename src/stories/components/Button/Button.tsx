d256e03755ffb2976869029cd32a503901a0f6ba
Jota Santos <jailsonsantosdpge@outlook.com>
2025-05-06 13:19:46 -0400

    4.8. Recebendo todas as props de um button dentro do componente personalizado

--- src/stories/components/Button/Button.tsx
+++ src/stories/components/Button/Button.tsx
@@ -1,13 +1,16 @@
-import * as B from './Button.styles';
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+import * as B from './Button.styles'
 
-export interface ButtonProps {
-    variant: 'danger' | 'text' | 'primary',
-    label: string,
-    onClick: () => {}
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  variant: 'danger' | 'text' | 'primary'
+  label: string
 }
 
-export default function Button({variant, label}: ButtonProps) {
-    return <B.Wrapper variant={variant}>
-        {label}
-    </B.Wrapper>
+export default function Button ({ label, variant, ref, ...props }: ButtonProps) {
+  return <B.Wrapper
+    {...props}
+    variant={variant}
+  >
+    { label }
+  </B.Wrapper>
 }
