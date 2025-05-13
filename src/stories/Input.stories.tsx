7fb830e177ecf5fea69125f90eb709512588be10
Jota Santos <jailsonsantosdpge@outlook.com>
2025-05-13 16:28:13 -0400

    4.10. Criando a API do Input

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Input, { InputProps } from './components/Input/Input';
+
+// Define o meta com tipagem
+const meta: Meta<typeof Input> = {
+  title: 'Example/Input',
+  component: Input,
+  parameters: {
+    layout: 'left',
+  },
+  tags: ['autodocs'],
+
+  // Você NÃO define args com tipo aqui.
+  // args é um objeto, não aceita anotação de tipo diretamente.
+};
+
+export default meta;
+
+// Aqui você usa a interface InputProps para os argumentos (args) das stories:
+type Story = StoryObj<InputProps>;
+
+export const WithPlaceholder: Story = {
+  args: {
+    label: 'e.g.: Jon Doe',
+    placeholder: 'Digite algo...',
+  },
+};
+
+export const WithLabelAndContent: Story = {
+  args: {  
+        label: 'Name',
+        value: 'Jon Doe'
+  },
+};
+
+
+export const WithContent: Story = {
+  args: {        
+        value: 'Jon Doe'
+  },
+};
+
---
+++ src/stories/components/Input/Input.tsx
@@ -0,0 +1,22 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import * as I from './Input.styles'; 
+
+export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    label?: string,
+}
+
+
+export default function Input({ label, ref, ...props }: InputProps) {
+    return <I.Wrapper>
+      {
+        label && 
+        <span>
+            { label }
+        </span>
+      }
+      <input 
+      { ...props }
+        type="text" 
+      />
+    </I.Wrapper>
+}
