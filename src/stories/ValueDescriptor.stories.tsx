2c6136a9dd4202c2acf832da293ddd0dbe9abbb8
Jota Santos <jailsonsantosdpge@outlook.com>
2025-05-28 12:51:05 -0400

    4.14. Uma nova forma de renderizar componentes de forma condicional

--- src/stories/ValueDescriptor.stories.tsx
+++ src/stories/ValueDescriptor.stories.tsx
@@ -1,53 +1,54 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import ValueDescriptor,  { ValueDesciptorProps } from '../components/ValueDescriptor/ValueDescriptor' 
+import ValueDescriptor, {
+  ValueDescriptorProps,
+} from "../components/ValueDescriptor/ValueDescriptor";
 
 // Define o meta com tipagem
 const meta: Meta<typeof ValueDescriptor> = {
-  title: 'Example/Value Descriptor',
+  title: "Example/Value Descriptor",
   component: ValueDescriptor,
   parameters: {
-    layout: 'left',
+    layout: "left",
   },
-  tags: ['autodocs']
+  tags: ["autodocs"],
 };
 
 export default meta;
 
 // Aqui você usa a interface InputProps para os argumentos (args) das stories:
-type Story = StoryObj<ValueDesciptorProps>;
+type Story = StoryObj<ValueDescriptorProps>;
 
 export const Default: Story = {
   args: {
-     description: 'Ganhos na semana',
-    value: 560332.02
+    description: "Ganhos na semana",
+    value: 560332.02,
   },
 };
 
 export const Primary: Story = {
   args: {
-    description: 'Ganhos na semana',
-    value: 560332.02
+    description: "Ganhos na semana",
+    value: 560322.02,
+    isCurrency: true,
+    color: "default",
   },
 };
-
-
 
 export const DefaultCurrency: Story = {
   args: {
-   description: 'Ganhos na semana',
-    value: 560332.02
+    description: "Ganhos na semana",
+    value: 560322.02,
+    isCurrency: false,
+    color: "primary",
   },
 };
-
 
 export const PrimaryCurrency: Story = {
   args: {
-  description: 'Ganhos na semana',
-    value: 560332.02
+    description: "Ganhos na semana",
+    value: 560322.02,
+    isCurrency: true,
+    color: "primary",
   },
 };
-
-
-
-
